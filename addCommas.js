1627e373805d97575f077a8d493732e76a260e78
Josue Aguirre <113935346+josue4433@users.noreply.github.com>
2023-08-25 10:38:43 -0400

    addCommas.js

--- addCommas.js
+++ addCommas.js
@@ -1,8 +1,25 @@
 function addCommas(number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-console.log(addCommas(1000)); // Output: "1,000"
-console.log(addCommas(1000000)); // Output: "1,000,000"
-console.log(addCommas(1234567890)); // Output: "1,234,567,890"
+  // Convert the number to a string
+  let numStr = number.toString();
 
-module.exports = addCommas;
+  // Split the number into integer and decimal parts (if applicable)
+  let parts = numStr.split('.');
+  let integerPart = parts[0];
+  let decimalPart = parts[1] || '';
+
+  // Add commas to the integer part
+  let formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+  // Combine integer and decimal parts with optional decimal point
+  let result = decimalPart ? `${formattedInteger}.${decimalPart}` : formattedInteger;
+
+  return result;
+}
+
+// Test cases
+console.log(addCommas(1234));          // Output: "1,234"
+console.log(addCommas(1000000));       // Output: "1,000,000"
+console.log(addCommas(9876543210));    // Output: "9,876,543,210"
+console.log(addCommas(6));             // Output: "6"
+console.log(addCommas(-10));           // Output: "-10"
+console.log(addCommas(-5678));         // Output: "-5,678"
